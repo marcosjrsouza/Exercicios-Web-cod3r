5623e204027e907f9da98bdbe6c032a1c68c78b5
Marcos <marcosjrsouza@hotmail.com>
2022-07-13 15:03:40 -0300

    terceira mudança

---
+++ most-used-words/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <div ></div>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+    data: function () {
+        return {
+            groupedWords: [
+              { name: 'i', amount: 1234 },
+              { name: 'you', amount: 900 },
+              { name: 'he', amount: 853 },
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
